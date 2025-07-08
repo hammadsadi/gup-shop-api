51b22e0933a0adc6bd38226f58fdcf748f89dbac
hammadsadi <hammad.sadi@yahoo.com>
2025-07-09 03:04:39 +0600

    feat: Implement Active User Tracking with Socket.IO

--- src/app/sockets/index.ts
+++ src/app/sockets/index.ts
@@ -2,6 +2,9 @@ import { Server } from "socket.io";
 import { messageHandler } from "./handlers/messageHandler";
 // import { userHandler } from "./handlers/userHandler";
 
+// Set Active User an Array
+let activeUsers: any = [];
+// Create a new instance of the Socket.IO server
 export const socketHandler = (io: Server) => {
   io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
@@ -9,9 +12,24 @@ export const socketHandler = (io: Server) => {
     // Handle message events
     messageHandler(io, socket);
     // userHandler(io, socket);
-
+    // Get User
+    socket.on("newUser", (data) => {
+      // Add the new user to the active users array
+      const isExisting = activeUsers.some((user: any) => user.data === data);
+      if (!isExisting) {
+        activeUsers.push({ user: data, socketId: socket.id });
+      }
+      // Emit the updated active users to all connected clients
+      io.emit("getAllActiveUsers", activeUsers);
+    });
     socket.on("disconnect", () => {
       console.log("Client disconnected", socket.id);
+      // Remove the disconnected user from the active users array
+      activeUsers = activeUsers.filter(
+        (user: any) => user.socketId !== socket.id
+      );
+      // Emit the updated active users to all connected clients
+      io.emit("getAllActiveUsers", activeUsers);
     });
   });
 };
